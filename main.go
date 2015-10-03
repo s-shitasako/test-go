857cc775f69175a16d2f915d10ec81386f8f0ddb
s-shitasako <s-shitasako@nekojarashi.com>
2015-10-03 09:57:14 +0900

    Separate headers and data

--- main.go
+++ main.go
@@ -10,14 +10,12 @@ import (
 func main() {
   ln, err := net.Listen("tcp", ":4000")
   if err != nil {
-    fmt.Println("Error:")
-    fmt.Println(err)
+    fmt.Println("Error:", err)
   } else {
     for {
       conn, err := ln.Accept()
       if err != nil {
-        fmt.Println("Error:")
-        fmt.Println(err)
+        fmt.Println("Error:", err)
         os.Exit(1)
       }
       go serve(conn)
@@ -25,15 +23,22 @@ func main() {
   }
 }
 
-func serve(c net.Conn) {
+func serve(conn net.Conn) {
+  defer func(){
+    if err := conn.Close(); err != nil {
+      fmt.Println("conn closed with err: ", err)
+    }
+  }()
   buf := make([]byte, 65536)
-  l, err := c.Read(buf)
+  l, err := conn.Read(buf)
   if l > 0 && err == nil {
-    s := "HTTP/1.1 200 OK\r\nContent-Length: 12\r\nContent-Type: text/html;charset=utf-8\r\nConnection: close\r\n\r\n<h1>あ</h1>"
+    data := []byte{'<', 'h', '1', '>', 0xe3, 0x81, 0x84, '<', '/', 'h', '1', '>'}
+    s := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Length: %d\r\nContent-Type: text/html;charset=utf-8\r\nConnection: close\r\n\r\n", len(data))
     sr := strings.NewReader(s)
     buf2 := make([]byte, sr.Len())
     sr.Read(buf2)
-    c.Write(buf2)
+    if _, err := conn.Write(buf2); err == nil {
+      conn.Write(data)
+    }
   }
-  fmt.Println("Connection closed. with err:", c.Close())
 }
